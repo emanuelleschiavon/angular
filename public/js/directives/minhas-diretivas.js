27452ff68e5aab7cc83e005ab40c2ef4dac0d77e
Emanuelle Schiavon <emanuelleschiavon@gmail.com>
2018-01-17 14:08:40 -0200

    Commit inicial

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,16 @@
+angular.module('minhasDiretivas', []).directive('meuPainel', function(){
+
+	var ddo = {};
+	
+	ddo.restrict = "AE";
+
+	ddo.scope = {
+		titulo: '@'
+	};
+
+	ddo.templateUrl = "js/directives/meu-painel.html";
+
+	ddo.transclude = true;
+	
+	return ddo;
+});
